c3c752419d471b49db81429f309609868f22812c
Jalonzpa <joshuaal22@g.eips.ca>
2018-04-21 17:07:33 -0600

    Random duration and volume

--- music.py
+++ music.py
@@ -1,17 +1,18 @@
 import pyaudio
 import numpy as np
 from random import randint
+from random import uniform
 p = pyaudio.PyAudio()
 
 print("How many notes do you want there to be in the entire piece?")
 choice = int(input())
 
-volume = 0.5
-fs = 44100
-duration = 0.5
+fs = 100000
 
 for i in range(choice):
-	f = randint(250,2000)
+	volume = uniform(0.1,1.0)
+	duration = uniform(0.25,2.0)
+	f = randint(100,2000)
 	print(f)
 	samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
 	stream = p.open(format=pyaudio.paFloat32,
