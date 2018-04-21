ea80d509c71c2f7809b8fa334b17f0acf1aec20e
Jalonzpa <joshuaal22@g.eips.ca>
2018-04-21 17:13:32 -0600

    Added comments

--- music.py
+++ music.py
@@ -5,22 +5,23 @@ from random import uniform
 p = pyaudio.PyAudio()
 
 print("How many notes do you want there to be in the entire piece?")
-choice = int(input())
+choice = int(input())	# stores an int from user in var choice
 
-fs = 100000
+fs = 100000	# sampling rate in hertz
 
-for i in range(choice):
-	volume = uniform(0.1,1.0)
-	duration = uniform(0.25,2.0)
-	f = randint(100,2000)
+for i in range(choice):	   # iterates over the int chosen by the user
+	volume = uniform(0.1,1.0)	# chooses random float for the volume
+	duration = uniform(0.25,2.0)	# chooses random float for the duration
+	f = randint(100,2000)	# chooses random int for the frequency
 	print(f)
-	samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+	samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)	# generates samples and changes it to a float32 array
 	stream = p.open(format=pyaudio.paFloat32,
 			channels=1,
 			rate=fs,
 			output=True)
 
-	stream.write(volume*samples)
+	stream.write(volume*samples)	# plays the sound
+
 	stream.stop_stream()
 	stream.close()
 
